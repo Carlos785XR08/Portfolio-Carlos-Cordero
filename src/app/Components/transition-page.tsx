bb3827c558e267b92bf317e4865c891ef46f7c34
Carlos Arturo Cordero Núñez <carloscordero4554@outlook.com>
2026-04-18 02:12:59 -0600

    cuarto actualización

--- src/app/Components/transition-page.tsx
+++ src/app/Components/transition-page.tsx
@@ -8,7 +8,7 @@ const TransitionPage = () => {
     <AnimatePresence mode="wait">
       <div>
         <motion.div
-          className="fixed top-0 bottom-0 right-full w-screen z-30 bg-[#00EBC7]"
+          className="fixed top-0 bottom-0 right-full w-screen z-30 bg-[#61DAFB]"
           variants={transitionVariantsPage}
           initial="initial"
           animate="animate"
