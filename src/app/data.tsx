d3eb6dec7975be8078f0ca13707d3e088fcbe7e8
Carlos Arturo Cordero Núñez <carloscordero4554@outlook.com>
2026-06-03 21:55:36 -0600

    cambios

--- src/app/data.tsx
+++ src/app/data.tsx
@@ -173,9 +173,12 @@ export const dataPortfolio = [
  {
         id: 3,
         title: "Diseño en Figma / E-commerce",
-        image: "/image-3.jpg",
+        image: "/images/E-commerce.png",
         urlGithub: "https://www.figma.com/proto/X6MttiCxdReIeVhXBkOT6g/Sin-t%C3%ADtulo?node-id=7-2&t=xhKcDwBCBDByW93b-0&scaling=scale-down&content-scaling=fixed&page-id=0%3A1",
         urlDemo: "https://www.figma.com/proto/X6MttiCxdReIeVhXBkOT6g/Sin-t%C3%ADtulo?node-id=7-2&t=xhKcDwBCBDByW93b-0&scaling=scale-down&content-scaling=fixed&page-id=0%3A1",
+        description:
+      "Prototipo móvil de compra en Figma: catálogo → producto → datos → pago → confirmación.",
+        technologies: ["FIGMA", ]
     },
     /*{
         id: 4,
